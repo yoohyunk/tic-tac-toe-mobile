a252b3fea9c948fe6856709be6061ac1f1bb3886
yoohyunk <163964321+yoohyunk@users.noreply.github.com>
2025-03-10 19:17:28 -0600

    redesigned sign in page (#3)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,15 @@
-import { Stack } from "expo-router";
+import { useEffect } from "react";
+import { useRouter, Slot, usePathname } from "expo-router";
 
-export default function Layout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+export default function RootLayout() {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (pathname !== "/signIn") {
+      router.replace("/signIn"); // Redirect to Sign-In on app start
+    }
+  }, []);
+
+  return <Slot />;
 }
